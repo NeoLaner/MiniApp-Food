ad1d8fe54d32881e594150dd3f172810d740fecb
neolaner <neo.laner.official@gmail.com>
2024-04-28 19:20:25 +0330

    add welcome feautre

--- src/features/welcome/Welcome.tsx
+++ src/features/welcome/Welcome.tsx
@@ -1,4 +1,4 @@
-import { useThemeParams } from "@vkruglikov/react-telegram-web-app";
+import { MainButton, useThemeParams } from "@vkruglikov/react-telegram-web-app";
 import { CardHeader, CardContent, CardFooter, Card, Cards } from "./Card";
 import Button from "./Button";
 import { useState } from "react";
@@ -75,12 +75,13 @@ export default function Component() {
             </div>
           </CardContent>
           <CardFooter className="pt-4">
-            <Button
+            <MainButton text="Get Started" onClick={() => setCurrentSlide(1)} />
+            {/* <Button
               onClick={() => setCurrentSlide(1)}
               className="inline-flex h-10 w-full items-center justify-center rounded-md bg-gray-900 px-4 py-2 text-sm font-medium text-gray-50 shadow transition-colors hover:bg-gray-900/90 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-gray-950 disabled:pointer-events-none disabled:opacity-50 dark:bg-gray-50 dark:text-gray-900 dark:hover:bg-gray-50/90 dark:focus-visible:ring-gray-300"
             >
               Get Started
-            </Button>
+            </Button> */}
           </CardFooter>
         </Card>
       </Cards>
