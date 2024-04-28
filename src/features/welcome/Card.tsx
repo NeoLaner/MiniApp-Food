2e00d82051ae4a4aaa8f83213c8e47227d26acf1
neolaner <neo.laner.official@gmail.com>
2024-04-28 20:02:11 +0330

    add welcome feautre

--- src/features/welcome/Card.tsx
+++ src/features/welcome/Card.tsx
@@ -1,12 +1,12 @@
 // src/components/ui/button/Button.tsx
 import React, { ReactNode } from "react";
 
-interface ButtonProps {
+interface BasicProps {
   className?: string;
   children: ReactNode;
 }
 
-export const Cards: React.FC<ButtonProps> = ({
+export const Cards: React.FC<BasicProps> = ({
   className = "",
   children,
   ...props
@@ -18,7 +18,53 @@ export const Cards: React.FC<ButtonProps> = ({
   );
 };
 
-export const Card: React.FC<ButtonProps> = ({
+interface CardProps {
+  index: number;
+  currentSlide: number;
+  headingText: string;
+  text: string;
+  image: string;
+}
+
+export const Card: React.FC<CardProps> = ({
+  index,
+  currentSlide,
+  headingText,
+  text,
+  image,
+}) => {
+  const style = {
+    transform: `translateX(${-(currentSlide - index) * 100}%)`,
+  };
+  return (
+    <div
+      style={style}
+      className={`w-full max-w-md transition-all ${
+        index !== currentSlide ? "absolute" : ""
+      }`}
+    >
+      <CardHeader className="relative overflow-hidden rounded-t-lg flex items-center justify-center ">
+        <img
+          alt="Welcome"
+          className="w-48"
+          height="200"
+          src={image}
+          width="400"
+        />
+      </CardHeader>
+      <CardContent className="space-y-4 p-6 text-center">
+        <div className="space-y-2">
+          <h2 className="text-2xl font-bold tracking-tighter text-[var(--tg-theme-text-color)]">
+            {headingText}
+          </h2>
+          <p className="text-[var(--tg-theme-text-color)] ">{text}</p>
+        </div>
+      </CardContent>
+    </div>
+  );
+};
+
+export const CardHeader: React.FC<BasicProps> = ({
   className = "",
   children,
   ...props
@@ -30,31 +76,7 @@ export const Card: React.FC<ButtonProps> = ({
   );
 };
 
-export const CardHeader: React.FC<ButtonProps> = ({
-  className = "",
-  children,
-  ...props
-}) => {
-  return (
-    <div className={`${className}`} {...props}>
-      {children}
-    </div>
-  );
-};
-
-export const CardContent: React.FC<ButtonProps> = ({
-  className = "",
-  children,
-  ...props
-}) => {
-  return (
-    <div className={`${className}`} {...props}>
-      {children}
-    </div>
-  );
-};
-
-export const CardFooter: React.FC<ButtonProps> = ({
+export const CardContent: React.FC<BasicProps> = ({
   className = "",
   children,
   ...props
--- src/features/welcome/Welcome.tsx
+++ src/features/welcome/Welcome.tsx
@@ -1,7 +1,16 @@
 import { MainButton, useThemeParams } from "@vkruglikov/react-telegram-web-app";
-import { CardHeader, CardContent, Card, Cards } from "./Card";
+import { Card, Cards } from "./Card";
 // import Button from "./Button";
 import { useState } from "react";
+
+const cards = [
+  { headingText: "Welcome", text: "Hey", image: "coock.webp" },
+  {
+    headingText: "Finish",
+    text: "Hey HeyHeyHeyHeyHeyHey",
+    image: "buy.webp",
+  },
+];
 
 export default function Component() {
   const [currentSlide, setCurrentSlide] = useState(0);
@@ -14,75 +23,16 @@ export default function Component() {
       className={`flex h-[100dvh] items-center justify-center bg-[var(--tg-theme-bg-color)]`}
     >
       <Cards className="flex overflow-hidden relative">
-        <Card
-          className={`w-full max-w-md   transition-all  ${
-            currentSlide > 0 ? "-translate-x-full" : ""
-          } `}
-        >
-          <CardHeader className="relative overflow-hidden rounded-t-lg flex items-center justify-center ">
-            <img
-              alt="Welcome"
-              className="w-48"
-              height="200"
-              src="coock.webp"
-              width="400"
-            />
-          </CardHeader>
-          <CardContent className="space-y-4 p-6 text-center">
-            <div className="space-y-2">
-              <h2 className="text-2xl font-bold tracking-tighter text-[var(--tg-theme-text-color)]">
-                Welcome to our App
-              </h2>
-              <p className="text-[var(--tg-theme-text-color)] ">
-                Get started by exploring our features and discovering how our
-                app can help you achieve your goals.
-              </p>
-            </div>
-          </CardContent>
-          {/* <CardFooter className="pt-4">
-            <Button
-              onClick={() => setCurrentSlide(1)}
-              className="inline-flex h-10 w-full items-center justify-center rounded-md bg-gray-900 px-4 py-2 text-sm font-medium text-gray-50 shadow transition-colors hover:bg-gray-900/90 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-gray-950 disabled:pointer-events-none disabled:opacity-50 dark:bg-gray-50 dark:text-gray-900 dark:hover:bg-gray-50/90 dark:focus-visible:ring-gray-300"
-            >
-              Get Started
-            </Button>
-          </CardFooter> */}
-        </Card>
-
-        <Card
-          className={`w-full max-w-md transition-all absolute ${
-            currentSlide > 0 ? "" : "translate-x-full"
-          } `}
-        >
-          <CardHeader className="relative overflow-hidden rounded-t-lg flex items-center justify-center">
-            <img
-              alt="Welcome"
-              className="w-48"
-              height="200"
-              src="buy.webp"
-              width="400"
-            />
-          </CardHeader>
-          <CardContent className="space-y-4 p-6 text-center">
-            <div className="space-y-2">
-              <h2 className="text-2xl font-bold tracking-tighter">
-                Welcome to our App
-              </h2>
-              <p className="text-gray-500 dark:text-gray-400">
-                Get started by exploring our features and discovering how our
-                app can help you achieve your goals.
-              </p>
-            </div>
-          </CardContent>
-          {/* <CardFooter className="pt-4">
-            <Button
-              onClick={() => setCurrentSlide(1)}
-              className="inline-flex h-10 w-full items-center justify-center rounded-md bg-gray-900 px-4 py-2 text-sm font-medium text-gray-50 shadow transition-colors hover:bg-gray-900/90 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-gray-950 disabled:pointer-events-none disabled:opacity-50 dark:bg-gray-50 dark:text-gray-900 dark:hover:bg-gray-50/90 dark:focus-visible:ring-gray-300"
-            >
-              Get Started
-            </Button>
-          </CardFooter> */}
-        </Card>
+        {cards.map((card, index) => (
+          <Card
+            index={index}
+            currentSlide={currentSlide}
+            headingText={card.headingText}
+            text={card.text}
+            image={card.image}
+            key={index}
+          />
+        ))}
       </Cards>
       <MainButton text="Get Started" onClick={() => setCurrentSlide(1)} />
     </main>
